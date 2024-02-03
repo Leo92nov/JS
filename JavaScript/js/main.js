c43e10f522f0b9a5a59302d28fedd3bf8ac486a6
Leonardo Nonino <lnonino92@gmail.com>
2024-02-02 23:43:16 -0300

    Primera entrega 2

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,6 +1,6 @@
 let dni= 36765496;
 while (true){
-    let ingreso = prompt("Ingrese su DNI para ver su resumen");
+    let ingreso = prompt("Ingrese su DNI para realizar su relevamiento");
     ingreso=parseInt(ingreso)
     if (ingreso===dni) {
         alert("Bienvenido Leonardo");
@@ -40,28 +40,27 @@ console.log("Restando todos sus gastos, su sueldo es de $" + sueldoFinal);
 if(sueldoFinal < 0){
     alert("Cuidado, estás perdiendo dinero!!!");
 }
-if(sueldoFinal===0){
+else if(sueldoFinal===0){
     alert("Tus gastos son iguales a tu entrada de dinero, intenta disminuirlos para poder ahorrar!")
-}
-
-while (sueldoFinal > 0){
+}else{
+    while (sueldoFinal > 0){
     let sueldoAhorro= prompt("¿Qué porcentaje de tu excedente queres ahorrar? \n 1-10% \n 2-20% \n 3-30% \n 4-40% \n 5-50% \n 6-No quiero ahorrar")
     sueldoAhorro=parseFloat(sueldoAhorro)
     switch (sueldoAhorro) {
         case 1:
-            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.1));
+            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.1) + " Disponiendo de $" + (sueldoFinal - sueldoFinal*0.1) + " restantes");
             break;
         case 2:
-            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.2));
+            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.2) + " Disponiendo de $" + (sueldoFinal - sueldoFinal*0.2) + " restantes");
             break;
         case 3:
-            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.3));
+            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.3) + " Disponiendo de $" + (sueldoFinal - sueldoFinal*0.3) + " restantes");
             break;
         case 4:
-            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.4));
+            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.4) + " Disponiendo de $" + (sueldoFinal - sueldoFinal*0.4) + " restantes");
             break;
         case 5:
-            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.5));
+            console.log("Tus ahorros mensuales serán de $" + (sueldoFinal * 0.5) + " Disponiendo de $" + (sueldoFinal - sueldoFinal*0.5) + " restantes");
             break;
         case 6:
             alert("Recuerda que es importante disponer de ahorros siempre que puedas permitírtelo");
@@ -69,6 +68,7 @@ while (sueldoFinal > 0){
         default:
             alert("Opción no válida, por favor elija la cantidad que desea ahorrar");
             continue;
-    }
-    break
+        }   
+        break
+    }   
 }
