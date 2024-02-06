eadc850083a33c6485ca6f17a227ac6fb5772f39
Leonardo Nonino <lnonino92@gmail.com>
2024-02-06 18:03:16 -0300

    entrega1.4

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,54 +1,63 @@
-let dni= 36765496;
-while (true){
-    let ingreso = prompt("Ingrese su DNI para realizar su relevamiento");
-    ingreso=parseInt(ingreso)
-    if (ingreso===dni) {
-        alert("Bienvenido Leonardo");
-        break 
-    } else{
-    alert("Usuario no registrado")
-    }
-continue
+let usuario= "36765496";
+let pass = "starplatinum";
+let intentos = 3;
+let ingreso=false
+function loggear() {
+    for (let i = 0; i<3; i++) {
+       let ingresoUsuario = prompt("Ingrese su DNI");
+       let ingresoPassword = prompt("Ingrese su contraseña");
+       if(ingresoUsuario== usuario && ingresoPassword ==pass){
+           return true;
+       }else{
+           alert("Usuario o contraseña incorrecta. Intentos restantes: " + (intentos - i -1 ));
+       }
+   }
+   return false;
 }
-let ahorrosPasados = "265.5";
-ahorrosPasados=parseFloat(ahorrosPasados)
-
-let sueldoBruto = prompt("ingrese su sueldo");
-sueldoBruto=parseFloat(sueldoBruto)
-
-let sueldoNeto= (sueldoBruto - sueldoBruto * 0.21);
-sueldoNeto=parseFloat(sueldoNeto);
-
-console.log("Su sueldo despues de impuestos fijos es de $" + sueldoNeto);
-
-let impuestoGas= prompt("Ingrese el valor de la factura de gas")
-let impuestoLuz= prompt("Ingrese el valor de la factura de luz")
-let impuestoTgi= prompt("Ingrese el valor de la factura de TGI")
-
-impuestoGas = parseFloat(impuestoGas);
-impuestoLuz = parseFloat(impuestoLuz);
-impuestoTgi = parseFloat(impuestoTgi);
-
-function sumaImpuestos(impuestoGas, impuestoLuz, impuestoTgi) {
-    return impuestoGas + impuestoLuz + impuestoTgi;
-}
-let totalImpuestos = sumaImpuestos(impuestoGas, impuestoLuz, impuestoTgi)
-totalImpuestos=parseFloat(totalImpuestos)
-console.log("La suma de los impuestos variables es $" + totalImpuestos);
-
-let sueldoFinal= (sueldoNeto - totalImpuestos)
-sueldoFinal=parseFloat(sueldoFinal)
-
-console.log("Restando todos sus gastos, su sueldo es de $" + sueldoFinal);
-
-let resultadoAhorro=0;
-resultadoAhorro=parseFloat(resultadoAhorro)
-if(sueldoFinal < 0){
-    alert("Cuidado, estás perdiendo dinero!!!");
-}
-else if(sueldoFinal===0){
-    alert("Tus gastos son iguales a tu entrada de dinero, intenta disminuirlos para poder ahorrar!")
+ingreso = loggear();
+if(!ingreso){
+   alert ("Haz superado el numero de intentos de ionicio de sesión, por favor intentalo nuevamente mas tarde")
 }else{
+    alert("Bienvenido Leonardo")
+    let ahorrosPasados = "265.5";
+    ahorrosPasados=parseFloat(ahorrosPasados)
+
+    let sueldoBruto = prompt("ingrese su sueldo");
+    sueldoBruto=parseFloat(sueldoBruto)
+
+    let sueldoNeto= (sueldoBruto - sueldoBruto * 0.21);
+    sueldoNeto=parseFloat(sueldoNeto);
+
+    console.log("Su sueldo despues de impuestos fijos es de $" + sueldoNeto);
+
+    let impuestoGas= prompt("Ingrese el valor de la factura de gas")
+    let impuestoLuz= prompt("Ingrese el valor de la factura de luz")
+    let impuestoTgi= prompt("Ingrese el valor de la factura de TGI")
+
+    impuestoGas = parseFloat(impuestoGas);
+    impuestoLuz = parseFloat(impuestoLuz);
+    impuestoTgi = parseFloat(impuestoTgi);
+
+    function sumaImpuestos(impuestoGas, impuestoLuz, impuestoTgi) {
+    return impuestoGas + impuestoLuz + impuestoTgi;
+    }
+    let totalImpuestos = sumaImpuestos(impuestoGas, impuestoLuz, impuestoTgi)
+    totalImpuestos=parseFloat(totalImpuestos)
+    console.log("La suma de los impuestos variables es $" + totalImpuestos);
+
+    let sueldoFinal= (sueldoNeto - totalImpuestos)
+    sueldoFinal=parseFloat(sueldoFinal)
+
+    console.log("Restando todos sus gastos, su sueldo es de $" + sueldoFinal);
+
+    let resultadoAhorro=0;
+    resultadoAhorro=parseFloat(resultadoAhorro)
+    if(sueldoFinal < 0){
+        alert("Cuidado, estás perdiendo dinero!!!");
+    }
+    else if(sueldoFinal===0){
+        alert("Tus gastos son iguales a tu entrada de dinero, intenta disminuirlos para poder ahorrar!")
+    }else{
     while (sueldoFinal > 0){
     let sueldoAhorro= prompt("¿Qué porcentaje de tu excedente queres ahorrar? \n 1-10% \n 2-20% \n 3-30% \n 4-40% \n 5-50% \n 6-No quiero ahorrar")
     sueldoAhorro=parseFloat(sueldoAhorro)
@@ -82,10 +91,11 @@ else if(sueldoFinal===0){
         }   
         break
     }
+    }
+    function sumaAhorros(resultadoAhorro, ahorrosPasados) {
+        return resultadoAhorro + ahorrosPasados;
+    }
+    let totalAhorros = sumaAhorros(resultadoAhorro, ahorrosPasados)
+    totalAhorros= parseFloat(totalAhorros)
+    console.log("Sus ahorros actualizados suman un total de $" + (totalAhorros));
 }
-function sumaAhorros(resultadoAhorro, ahorrosPasados) {
-    return resultadoAhorro + ahorrosPasados;
-}
-let totalAhorros = sumaAhorros(resultadoAhorro, ahorrosPasados)
-totalAhorros= parseFloat(totalAhorros)
-console.log("Sus ahorros actualizados suman un total de $" + (totalAhorros));
