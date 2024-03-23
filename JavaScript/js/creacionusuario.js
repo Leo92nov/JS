b89410aca7942badbef3eac8141951beb8fb147c
Leonardo Nonino <lnonino92@gmail.com>
2024-03-23 15:55:57 -0300

    Entrega3.1

--- JavaScript/js/creacionusuario.js
+++ JavaScript/js/creacionusuario.js
@@ -1,17 +1,21 @@
-usuarios = JSON.parse(localStorage.getItem("suarios"))
+let usuarios = JSON.parse(localStorage.getItem("suarios"));
+console.log(usuarios);
 
-let usuarioJ = JSON.parse(localStorage.getItem("usuarios"));
 
 const NOMBRE = document.getElementById("nombreUsuarioN")
 const MAIL = document.getElementById("mailUsuarioN");
 const USER = document.getElementById("userN");
 const PASS = document.getElementById("passwordUsuarioN");
 const BTNUSUARION = document.getElementById("btnCrear");
+const BTNVOLVER = document.getElementById("btnVolver")
 
-function crearUser(mail, user, pass) {
-    this.mail = mail;
-    this.user = user;
-    this.pass = pass;
+
+class crearUser{
+    constructor(mail, user, pass) {
+        this.mail = mail;
+        this.user = user;
+        this.pass = pass;
+    }
 }
 
 BTNUSUARION.addEventListener("click", function() {
@@ -20,10 +24,20 @@ BTNUSUARION.addEventListener("click", function() {
     const userValue = USER.value;
     const passValue = PASS.value;
     const nuevoUsuario = new crearUser(mailValue, userValue, passValue);
+
  console.log(nuevoUsuario);
  usuarios.push(nuevoUsuario)
+ console.log(usuarios);
 
-    localStorage.setItem("suarios", JSON.stringify(usuarios));
-    /* location.href = "../index.html" */
- console.log(usuarioJ);
+
+let usuariosj = JSON.stringify(usuarios);
+    localStorage.setItem("suarios", usuariosj) 
+    alert("Usuario creado exitosamente")
+
 });
+
+BTNVOLVER.addEventListener("click", function(){
+        location.href = "../index.html";
+});
+
+
--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -27,55 +27,60 @@ let cajaDePuntas = [
     cajaDePuntas = JSON.stringify(cajaDePuntas);
     localStorage.setItem("cajadepuntas", cajaDePuntas)
 
-let usuarios = [
-    {nombre: "Leonardo", nombreUsuario: "36765496", contraseña: "starplatinum", ahorrosPasados: 265000, numeroCcomitente: 54687879,  cedearsEnCartera:  [
-        {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
-        {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
-        {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
-        {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
-        {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
-        {id: 6, ticker: "META", precio: 2000, cantidad: 31},
-        {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},
-    ] },
-    {nombre: "Leonidas", nombreUsuario: "papitomirey", contraseña: "xerxes", ahorrosPasados: 2650000, numeroCcomitente: 54687880, cedearsEnCartera: [
-        {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
-        {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
-        {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
-        {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
-        {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
-        {id: 6, ticker: "META", precio: 2000, cantidad: 31},
-        {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},
-    ]},
-    {nombre: "Rolinga", nombreUsuario: "pepeluis", contraseña: "vaporlaciudad", ahorrosPasados: 285000, numeroCcomitente: 54687881, cedearsEnCartera: [
-        {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
-        {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
-        {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
-        {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
-        {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
-        {id: 6, ticker: "META", precio: 2000, cantidad: 31},
-        {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},]},
+usuarios = JSON.parse(localStorage.getItem("suarios"))
 
-    {nombre: "Tonatiw", nombreUsuario: "reysol", contraseña: "sacrificio", ahorrosPasados: 2650000, numeroCcomitente: 54687882, cedearsEnCartera: [
-        {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
-        {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
-        {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
-        {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
-        {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
-        {id: 6, ticker: "META", precio: 2000, cantidad: 31},
-        {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},]},
-
-    {nombre: "Otto", nombreUsuario: "acorazado", contraseña: "bismarck", ahorrosPasados: 25000, numeroCcomitente: 54687883, cedearsEnCartera: [
-        {id: 1, ticker: "KO", precio: 2000, cantidad: 2},
-        {id: 2, ticker:"PFE", precio: 1800, cantidad: 0},
-        {id: 3, ticker: "AAPL", precio: 2000, cantidad: 8},
-        {id: 4, ticker: "MSFT", precio: 2000, cantidad: 12},
-        {id: 5, ticker: "AMZN", precio: 2500, cantidad: 4},
-        {id: 6, ticker: "META", precio: 2000, cantidad: 1},
-        {id: 7, ticker: "TSLA", precio: 3000, cantidad: 5},]},
-]
-
- let usuariosJ = JSON.stringify(usuarios);
+if (usuarios === null) {
+    let usuarios = [
+        {nombre: "Leonardo", nombreUsuario: "36765496", contraseña: "starplatinum", ahorrosPasados: 265000, numeroCcomitente: 54687879,  cedearsEnCartera:  [
+            {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
+            {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
+            {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
+            {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
+            {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
+            {id: 6, ticker: "META", precio: 2000, cantidad: 31},
+            {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},
+        ] },
+        {nombre: "Leonidas", nombreUsuario: "papitomirey", contraseña: "xerxes", ahorrosPasados: 2650000, numeroCcomitente: 54687880, cedearsEnCartera: [
+            {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
+            {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
+            {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
+            {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
+            {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
+            {id: 6, ticker: "META", precio: 2000, cantidad: 31},
+            {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},
+        ]},
+        {nombre: "Rolinga", nombreUsuario: "pepeluis", contraseña: "vaporlaciudad", ahorrosPasados: 285000, numeroCcomitente: 54687881, cedearsEnCartera: [
+            {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
+            {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
+            {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
+            {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
+            {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
+            {id: 6, ticker: "META", precio: 2000, cantidad: 31},
+            {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},]},
+    
+        {nombre: "Tonatiw", nombreUsuario: "reysol", contraseña: "sacrificio", ahorrosPasados: 2650000, numeroCcomitente: 54687882, cedearsEnCartera: [
+            {id: 1, ticker: "KO", precio: 2000, cantidad: 12},
+            {id: 2, ticker:"PFE", precio: 1800, cantidad: 18},
+            {id: 3, ticker: "AAPL", precio: 2000, cantidad: 10},
+            {id: 4, ticker: "MSFT", precio: 2000, cantidad: 27},
+            {id: 5, ticker: "AMZN", precio: 2500, cantidad: 60},
+            {id: 6, ticker: "META", precio: 2000, cantidad: 31},
+            {id: 7, ticker: "TSLA", precio: 3000, cantidad: 15},]},
+    
+        {nombre: "Otto", nombreUsuario: "acorazado", contraseña: "bismarck", ahorrosPasados: 25000, numeroCcomitente: 54687883, cedearsEnCartera: [
+            {id: 1, ticker: "KO", precio: 2000, cantidad: 2},
+            {id: 2, ticker:"PFE", precio: 1800, cantidad: 0},
+            {id: 3, ticker: "AAPL", precio: 2000, cantidad: 8},
+            {id: 4, ticker: "MSFT", precio: 2000, cantidad: 12},
+            {id: 5, ticker: "AMZN", precio: 2500, cantidad: 4},
+            {id: 6, ticker: "META", precio: 2000, cantidad: 1},
+            {id: 7, ticker: "TSLA", precio: 3000, cantidad: 5},]},
+    ]
+let usuariosJ = JSON.stringify(usuarios)
     localStorage.setItem("suarios", usuariosJ)
+}
+
+    
 
 const btnIngreso = document.getElementById("btnIngreso");
 const inputUsuario = document.getElementById("name");
